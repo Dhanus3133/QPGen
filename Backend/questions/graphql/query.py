e02e932ed53b287602e2e932499bb37452c24429
Dhanus <dhanus3133@gmail.com>
2022-10-17 16:57:42 +0530

    Added GraphQL Types for question models

--- Backend/questions/graphql/query.py
+++ Backend/questions/graphql/query.py
@@ -1,25 +1,26 @@
+from typing import List
 import strawberry
 from strawberry.scalars import JSON
 from strawberry.types import Info
 from strawberry_django_plus import gql
+from django.contrib.auth.decorators import login_required
+
+from questions.graphql.types import QuestionType
 
 # from generate import generate_questions
 # from .types import HelloType
 
+
 @strawberry.type
 class HelloType:
-    hello: str
+    hello: JSON
     name: str
     age: int
-
-class HelloValue:
-    hello = "Hello Sir"
-    name = "Dhanus"
-    age = 18
 
 
 @gql.type
 class Query:
+    questions: List[QuestionType] = gql.django.field()
     @gql.django.field
     async def hello(self, info: Info) -> JSON:
         lids = [3, 4]
@@ -27,5 +28,8 @@ class Query:
         count = [5, 2, 1]
         choices = [False, True, True]
         # return generate_questions(lids, marks, count, choices)
-
-        return {"hello":"COme on"}
+        return {"hello": "COme on"}
+    
+    # @login_required
+    # @gql.django.field
+    # def 
--- Backend/questions/graphql/types.py
+++ Backend/questions/graphql/types.py
@@ -1,6 +1,123 @@
+from typing import List
 from strawberry_django.fields.types import JSON
 from strawberry_django_plus import gql
+from questions.models import BloomsTaxonomyLevel, Course, Degree, Department, FacultiesHandling, Lesson, MarkRange, PreviousYearsQP, Programme, Question, Regulation, Subject, Syllabus
+
+from users.graphql.types import UserType
+
 
 @gql.type
 class HelloType:
     message: JSON
+
+
+@gql.django.type(BloomsTaxonomyLevel)
+class BloomsTaxonomyLevelType(gql.Node):
+    id: gql.auto
+    name: gql.auto
+    description: gql.auto
+
+
+@gql.django.type(PreviousYearsQP)
+class PreviousYearsQPType(gql.Node):
+    id: gql.auto
+    month: gql.auto
+    year: gql.auto
+
+
+@gql.interface
+class NameInterface(gql.Node):
+    id: gql.auto
+    name: gql.auto
+
+
+@gql.django.type(Programme)
+class ProgrammeType(NameInterface):
+    pass
+
+
+@gql.django.type(Degree)
+class DegreeType(NameInterface):
+    full_form: gql.auto
+
+
+@gql.django.type(Regulation)
+class RegulationType(gql.Node):
+    id: gql.auto
+    year: gql.auto
+
+
+@gql.django.type(Department)
+class DepartmentType(gql.Node):
+    id: gql.auto
+    programme: ProgrammeType
+    degree: DegreeType
+    branch: gql.auto
+    branch_code: gql.auto
+    hod: UserType
+
+
+@gql.django.type(Subject)
+class SubjectType(gql.Node):
+    id: gql.auto
+    code: gql.auto
+    subject_name: gql.auto
+    co: gql.auto
+    co_description: gql.auto
+    course_outcome: gql.auto
+    coe: List[UserType]
+
+
+@gql.django.type(Lesson)
+class LessonType(gql.Node):
+    id: gql.auto
+    name: gql.auto
+    subject: SubjectType
+
+
+@gql.django.type(Course)
+class CourseType(gql.Node):
+    id: gql.auto
+    regulation: RegulationType
+    semester: gql.auto
+    department: DepartmentType
+    active: gql.auto
+
+
+@gql.django.type(Syllabus)
+class SyllabusType(gql.Node):
+    id: gql.auto
+    course: CourseType
+    unit: gql.auto
+    lesson: LessonType
+
+
+@gql.django.type(MarkRange)
+class MarkRangeType(gql.Node):
+    id: gql.auto
+    start: gql.auto
+    end: gql.auto
+
+
+@gql.django.type(FacultiesHandling)
+class FacultiesHandlingType(gql.Node):
+    id: gql.auto
+    course: CourseType
+    subject: SubjectType
+    faculties: List[UserType]
+
+
+@gql.django.type(Question)
+class QuestionType(gql.Node):
+    id: gql.auto
+    slug: gql.auto
+    lesson: LessonType
+    question: gql.auto
+    answer: gql.auto
+    mark: MarkRangeType
+    start_mark: gql.auto
+    end_mark: gql.auto
+    btl: BloomsTaxonomyLevelType
+    difficulty: gql.auto
+    created_by: UserType
+    previous_years: List[PreviousYearsQPType]
