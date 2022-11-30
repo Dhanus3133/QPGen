e56de37ad4f7c64f8d795e67084e027ffc7c7bd7
Dhanus <dhanus3133@gmail.com>
2022-11-30 15:31:35 +0530

    Custom AdminModel

--- Backend/users/admin.py
+++ Backend/users/admin.py
@@ -1,7 +1,39 @@
 from django.contrib import admin
+from django.contrib.auth.forms import AdminPasswordChangeForm
 from .models import User
+from django.contrib.auth.admin import UserAdmin as UserAdmin_
+from django.utils.translation import gettext_lazy as _
 
 
-@admin.register(User)
-class UserModelAdmin(admin.ModelAdmin):
-    pass
+class CustomUserAdmin(UserAdmin_):
+    list_display = ("email", "first_name", "last_name", "is_staff")
+    search_fields = ("first_name", "last_name", "email")
+    ordering = ("email",)
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2"),
+            },
+        ),
+    )
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+
+admin.site.register(User, CustomUserAdmin)
